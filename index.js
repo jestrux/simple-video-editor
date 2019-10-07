1c93d537501fad59748a6e70f0f9d4f735526479
Walter Kimaro <wakyj07@gmail.com>
2019-10-07 20:48:49 +0300

    Setup UI

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
-var ffmpeg = require('fluent-ffmpeg');
-console.log("Converting....");
-ffmpeg(__dirname + '/spider-man.mp4')
-    .setStartTime('00:19:12')
-    .setDuration('20')
-    .withVideoBitrate('900k')
-    .withSize('750x?')
-    .output(__dirname + '/bo.mp4')
-    .on('end', function(err) {
-        if(err)
-            console.log("Error clipping!", err);
-        if(!err)
-            console.log('conversion Done');          
-    })
-    .on('progress', function(progress){
-        console.log('Progress......' + progress.percent + '%');
-    })
-    .on('error', function(err){
-        console.log('error: ', err);
-    })
-    .run();
+const {menubar} = require('menubar');
+const {ipcMain} = require("electron");
+const ffmpeg = require('fluent-ffmpeg');
+
+const mb = menubar({
+    icon: 'icon.png',
+    browserWindow: {
+        // width: 1000, height: 460,
+        width: 650, height: 460,
+        transparent: true
+    },
+    preloadWindow: true,
+    webPreferences: {
+        nodeIntegration: true
+    }
+});
+
+// mb.on('after-create-window', () => mb.window.openDevTools())
+
+mb.on('ready', () => {
+    console.log('app is ready');
+});
+
+ipcMain.on('log', (event, content) => {
+    console.log(content);
+});
+
+ipcMain.on('save-video', (event, link) => {
+    //   clipboard.writeText(link, 'selection')
+});
+
+ipcMain.on('state-change', (event, state) => {
+    switch (state) {
+        case "video-loaded":
+        default:
+            mb.window.setSize(630, 460);
+            break;
+    }
+});
+
+function cutVideo(filePath = __dirname + "/friends.webm", startTime = "00:00:15", duration = "10", outputPath = __dirname + '/friends-cut.mp4') {
+    console.log("Cutting....");
+    ffmpeg(filePath)
+        .setStartTime(startTime)
+        .setDuration(duration)
+        .withVideoBitrate('900k')
+        .withSize('750x?')
+        .output(outputPath)
+        .on('end', function (err) {
+            if (err)
+                console.log("Error clipping!", err);
+            if (!err)
+                console.log('conversion Done');
+        })
+        .on('progress', function (progress) {
+            console.log('Progress......' + progress.percent + '%');
+        })
+        .on('error', function (err) {
+            console.log('error: ', err);
+        })
+        .run();
+}
---
+++ video-player.js
@@ -0,0 +1,93 @@
+Vue.component('video-player', {
+    template: '#video-player',
+    props: {
+        src: String,
+        startTime: Number,
+        endTime: Number,
+        loop: Boolean
+    },
+    async mounted(){
+        if(this.src && this.src.length){
+            this._getVideoDuration(this.src, (duration) => {
+                this.duration = duration;
+                this.$emit('duration', duration);
+                this.$refs.video.volume = 50;
+            });
+        }
+    },
+    data(){
+        return {
+            currentTime: 0,
+            duration: 0
+        }
+    },
+    watch: {
+        startTime: function(newValue){
+            const video = this.$refs.video;
+            if(video.currentTime < newValue)
+                video.currentTime = newValue;
+        },
+        endTime: function(newValue){
+            const video = this.$refs.video;
+            if(video.currentTime > newValue)
+                video.currentTime = newValue;
+        }
+    },
+    methods: {
+        setCurrentTime(time, updateVideo){
+            const video = this.$refs.video;
+            time = parseInt(time);
+            if(time > this.endTime)
+                return this.setCurrentTime(this.endTime, true);
+            else if(time < this.startTime)
+                return this.setCurrentTime(this.startTime, true);
+
+            this.currentTime = time;
+            if(updateVideo){
+                video.currentTime = time;
+            }
+
+            if(time === this.endTime && !this.loop)
+                video.pause();
+            else if(time === this.endTime && this.loop)
+                return this.setCurrentTime(this.startTime, true);
+            else if(time === this.startTime && this.loop && video.paused)
+                video.play();
+        },
+        togglePlay(){
+            const video = this.$refs.video;
+            if((video.paused && video.currentTime === this.endTime) || video.ended){
+                video.currentTime = this.startTime;
+                video.play();
+            }
+            else if(video.paused){
+                video.play();
+            }
+            else
+                video.pause();
+        },
+        formatTime(time = 0){
+            const hr  = Math.floor((time/3600));
+            const min = Math.floor(((time%3600)/60));
+            const sec = Math.floor((time%60));
+
+            return `${hr}:${min}:${sec}`.split(':')
+                    .map(t => t.padStart(2, '0'))
+                    .join(':');
+        },
+        _getVideoDuration(url, next) {
+            var _player = new Audio(url);
+            _player.addEventListener("durationchange", function (e) {
+                if (this.duration!=Infinity) {
+                   var duration = this.duration
+                   _player.remove();
+                   next(duration);
+                };
+            }, false);      
+            _player.load();
+            _player.currentTime = 24*60*60; //fake big time
+            _player.volume = 0;
+            _player.play();
+        }
+    }
+});
