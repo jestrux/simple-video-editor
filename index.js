166442b6a10bb62cba7ebb0057fb232bc0dd412b
Walter Kimaro <wakyj07@gmail.com>
2019-10-08 12:15:30 +0300

    Save generated file to downloads folder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {menubar} = require('menubar');
-const {ipcMain, Notification} = require("electron");
+const {ipcMain, Notification, app} = require("electron");
 const ffmpeg = require('fluent-ffmpeg');
+const path = require('path'), fs =require('fs');
 let mainApp = {}; // for things that shouldn't be garbage collected
 
 const mb = menubar({
@@ -30,7 +31,17 @@ ipcMain.on('cut-video', (_event, ...params) => {
     cutVideo(...params);
 });
 
-function cutVideo(filePath = __dirname + "/friends.webm", startTime = "00:00:15", duration = "10", outputPath = __dirname + '/friends-cut.mp4') {
+async function cutVideo(filePath, startTime, duration, outputPath) {
+    if(!outputPath){
+        const ext = '.' + filePath.split('.').pop();
+        let fileName = filePath.split('.');
+        fileName.pop();
+        fileName = fileName.join('.').split('/').pop() + " - chopped";
+        const matchingFiles = await matchingFilesInDir(app.getPath("downloads"), fileName);
+        fileName += (matchingFiles > 0) ? ' - ' + matchingFiles : '';
+        outputPath = app.getPath("downloads") + '/' + fileName + ext;
+    }
+    
     mb.window.webContents.send('cut-progress', 0);
     ffmpeg(filePath)
         .setStartTime(startTime)
@@ -38,22 +49,28 @@ function cutVideo(filePath = __dirname + "/friends.webm", startTime = "00:00:15"
         .withVideoBitrate('900k')
         .withSize('750x?')
         .output(outputPath)
-        .on('end', function (err) {
-            if (err){
-                console.log("Error clipping!", err);
+        .on('end', function (error) {
+            if (error){
+                console.log("Error clipping!", error);
                 mb.window.webContents.send('cut-error', 'Failed', error);
+
+                mainApp.notification = new Notification({
+                    title: 'Failed to cut video',
+                    body: error.toString()
+                });
+                mainApp.notification.show();
+
                 return;
             }
             
-            mb.window.webContents.send('cut-done', filePath);
-            console.log('cut done', filePath);
+            mb.window.webContents.send('cut-done', outputPath);
+            console.log('cut done', outputPath);
 
             mainApp.notification = new Notification({
                 title: 'Video was successfully cut',
                 body: 'You can find new cut video in your Downloads'
             });
             mainApp.notification.on("click", _ev => {
-                console.log("User Clicked on Notification");
                 mb.window.previewFile(outputPath);
             });
             mainApp.notification.show();
@@ -62,9 +79,27 @@ function cutVideo(filePath = __dirname + "/friends.webm", startTime = "00:00:15"
             mb.window.webContents.send('cut-progress', progress.percent);
             console.log('Progress......' + progress.percent + '%');
         })
-        .on('error', function (err) {
-            console.log('error: ', err);
+        .on('error', function (error) {
+            console.log('error: ', error);
+            mainApp.notification = new Notification({
+                title: 'Failed to cut video',
+                body: error.toString()
+            });
+            mainApp.notification.show();
             mb.window.webContents.send('cut-error', 'Failed', error);
         })
         .run();
 }
+
+function matchingFilesInDir(folder,filter){
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(folder)){
+            reject(`Folder: ${folder} not found!`);
+            return;
+        }
+    
+        const files=fs.readdirSync(folder);
+        const matchingFiles = files.filter(filename => filename.indexOf(filter) !== -1);
+        resolve(matchingFiles.length);
+    })
+};
